7753b1f916f6913137206e6e2fadbf509adc915c
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2021-08-31 16:33:11 +0530

    Add

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : "mongodb+srv://subhojit_dey:YMgQ2IYci4doDAOV@cluster0.pvyuv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoos = require("mongoose")
+const items = require('./routes/api/Item')
+const path = require('path')
+
+const app = express();
+app.use(express.json())
+
+const db = require("./config/keys").mongoURI;
+mongoos.connect(db,{ useNewUrlParser: true , useUnifiedTopology: true })
+.then(()=>console.log("...Connected"))
+.catch((err)=>console.log("Error",err))
+
+
+app.use('/api/items',items)
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port,()=>console.log(`Server started ${port}`))
