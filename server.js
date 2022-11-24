3420e8d467661ae8bb0887aed0878fd460d76f4f
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-11-24 10:47:24 +0530

    vercel

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const express = require("express");
-const mongoos = require("mongoose")
-const items = require('./routes/api/Item')
-const path = require('path')
+const mongoos = require("mongoose");
+const items = require("./routes/api/Item");
+const path = require("path");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const db = require("./config/keys").mongoURI;
-mongoos.connect(db,{ useNewUrlParser: true , useUnifiedTopology: true })
-.then(()=>console.log("...Connected"))
-.catch((err)=>console.log("Error",err))
+mongoos
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log("...Connected"))
+	.catch((err) => console.log("Error", err));
 
+app.get("/user", function (req, res) {
+	console.log("/user request called");
+	res.send("Welcome to GeeksforGeeks");
+});
+app.use("/api/items", items);
 
-app.use('/api/items',items)
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static('client/build'))
 
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//     })
+// }
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'))
+const port = process.env.PORT || 5000;
 
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
-
-
-const port = process.env.PORT || 5000
-
-app.listen(port,()=>console.log(`Server started ${port}`))
+app.listen(port, () => console.log(`Server started ${port}`));
