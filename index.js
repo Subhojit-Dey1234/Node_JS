93e820285adad1d452f4735dfe50a7ff8c759652
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-11-24 18:19:53 +0530

    vercel

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoos = require("mongoose");
 const items = require("./routes/api/Item");
 const path = require("path");
+const winston = require('winston');
+const logger = require('./logger')
 
 const app = express();
 app.use(express.json());
@@ -18,13 +20,20 @@ app.get("/user", function (req, res) {
 });
 app.use("/api/items", items);
 
-// if(process.env.NODE_ENV === "production"){
-//     app.use(express.static('client/build'))
 
-//     app.get('*',(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000;
 
--- routes/api/Item.js
+++ routes/api/Item.js
@@ -1,28 +1,35 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const logger = require("../../logger");
+const router = express.Router();
 
-const Item = require('../../models/Item')
+const Item = require("../../models/Item");
 
-router.get('/',(req,res)=>{
-    Item.find()
-    .sort({date: -1})
-    .then(items => res.json(items))
-})
+router.get("/", (req, res) => {
+	Item.find()
+		.sort({ date: -1 })
+		.then((items) => {
+            logger.info("Successfully.....")
+            res.json(items)
+        });
+});
 
+router.post("/", (req, res) => {
+	const newItem = new Item({
+		name: req.body.name,
+	});
 
-router.post('/',(req,res)=>{
-    const newItem = new Item({
-        name: req.body.name
-    })
+	newItem.save().then((item) => {
+        logger.info("Successfully.....")
+        res.json({ item: item, success: true })});
+});
 
-    newItem.save().then(item => res.json({item : item,success:true}));
-})
+router.delete("/:id", (req, res) => {
+	Item.findById(req.params.id)
+		.then((item) => item.remove().then(() => res.json({ success: true })))
+		.catch((err) => {
+            logger.error(err)
+            res.status(404).json({ success: false })
+        });
+});
 
-
-router.delete('/:id',(req,res)=>{
-    Item.findById(req.params.id)
-    .then(item=> item.remove().then(()=>res.json({success:true})))
-    .catch(err=>res.status(404).json({success:false}))
-})
-
-module.exports = router
+module.exports = router;
