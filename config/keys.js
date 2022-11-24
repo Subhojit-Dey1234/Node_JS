cf3e67c49d2b57d04c86ead6c22f563aa9e553ae
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-11-24 17:52:24 +0530

    vercel

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI : "mongodb+srv://subhojit_dey:YMgQ2IYci4doDAOV@cluster0.pvyuv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongoURI : "mongodb+srv://subhojitdey:u4AdZkTCZY1V3iOj@cluster0.gho61.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
